b7801b4cb2e56b4cd5c381610a53db0c1330c2e0
Suba79 <coolzaken13@gmail.com>
2026-05-22 22:44:01 +0300

    setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -16,6 +16,7 @@ class FlatAdmin(admin.ModelAdmin):
     )
     list_editable = ('new_building',)
     list_filter = ('new_building',)
+    raw_id_fields = ('liked_by',)
 
 
 @admin.register(Complaint)
--- property/models.py
+++ property/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
@@ -39,7 +40,11 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
 
-    has_balcony = models.BooleanField('Наличие балкона', null=True, blank=True, db_index=True)
+    has_balcony = models.BooleanField(
+        'Наличие балкона',
+        null=True,
+        blank=True,
+        db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
@@ -48,7 +53,14 @@ class Flat(models.Model):
         db_index=True)
 
     new_building = models.BooleanField(null=True, blank=True, db_index=True)
-    
+
+    liked_by = models.ManyToManyField(
+        User,
+        verbose_name='Кто лайкнул',
+        related_name='liked_flats',
+        blank=True,
+    )
+
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
